59aee498dabbe70de9632559328d58a6b6414b01
apecherin <artempecherin@gmail.com>
2014-03-29 19:33:02 +0400

    first commit

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,9 @@
+class IndexController < ApplicationController
+  def webcam1
+
+  end
+
+  def webcam2
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webcamviewer::Application.config.secret_key_base = '6f6aecfd20a537bbc28ab94afa67f9a96b7aade167090cc3f4e48052b1ef0dc30b1e2c14b1c56dc17a148c75781b65623b103ba0955b77d85a6d3b772f7a4a67'
